a6df15cdd4966a1ace599b6d778df993035e3dad
Gabriela <gabriela.cleims@gmail.com>
2024-01-23 17:46:59 -0300

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleTerms() {
+    var terms = document.getElementById('terms');
+    if (terms.style.display === "none") {
+        terms.style.display = "block";
+    } else {
+        terms.style.display = "none";
+    }
+}
